be364ee0f9164c220eba1f957cef36dff1b91786
Vincent WENDLING <vincent.wendling@lilo.org>
2023-11-15 17:25:17 +0100

    feat: started working on the canvas drawing

--- src/app/canva/canva.component.ts
+++ src/app/canva/canva.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,30 @@ import { CommonModule } from '@angular/common';
   templateUrl: './canva.component.html',
   styleUrl: './canva.component.css'
 })
-export class CanvaComponent {
+export class CanvaComponent implements AfterViewInit {
+  
+  @ViewChild('canvas', { static: false })
+  canvas!: ElementRef<HTMLCanvasElement>;
+  
+  public context!: CanvasRenderingContext2D;
 
+  ngAfterViewInit(): void {
+    if (this.canvas !== undefined) {
+      this.context = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+      // this.context.fillStyle = "green";
+      // this.context.fillRect(100, 100, 100, 100);
+    }
+  }
+
+  handleClick(e: MouseEvent): void {
+    
+    const x = e.clientX - this.canvas.nativeElement.getBoundingClientRect().left;
+    const y = e.clientY - this.canvas.nativeElement.getBoundingClientRect().top;
+
+    // Now, you can use x and y to perform actions based on the click position
+    console.log(`Clicked at (${x}, ${y})`);
+
+    this.context.fillStyle = 'blue';
+    this.context.fillRect(x, y, 100, 100);
+  }
 }
